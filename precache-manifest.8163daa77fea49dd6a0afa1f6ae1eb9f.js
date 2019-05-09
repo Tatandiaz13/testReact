8354a1cf2f407c20cc85c2246d752daf257231ec
Sebastian Diaz <johnn@elabogado.com>
2019-05-09 18:11:28 +0200

    Updates

---
+++ precache-manifest.8163daa77fea49dd6a0afa1f6ae1eb9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8954895ff4b74efb8274ea2fb20811c8",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "b4c4b97eb76104d92776",
+    "url": "/testReact/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "e43cd93afad5a7be1d9f",
+    "url": "/testReact/static/js/2.2348e927.chunk.js"
+  },
+  {
+    "revision": "b4c4b97eb76104d92776",
+    "url": "/testReact/static/js/main.1f571796.chunk.js"
+  },
+  {
+    "revision": "d4616c71270be19d2ccd",
+    "url": "/testReact/static/js/runtime~main.0ac0c888.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
